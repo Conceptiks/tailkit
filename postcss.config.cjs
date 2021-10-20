2a996cf84d630521c6019dab1d204714d151dbde
Maxim <maxim.tan@inlume.io>
2021-10-20 17:23:31 +0200

    Added adapter-node, and postcss plugins

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,5 +1,8 @@
 const tailwindcss = require('tailwindcss');
 const autoprefixer = require('autoprefixer');
+const atImport = require('postcss-import');
+const postcssNested = require('postcss-nested');
+const postcssPresetEnv = require('postcss-preset-env');
 const cssnano = require('cssnano');
 
 const mode = process.env.NODE_ENV;
@@ -7,8 +10,14 @@ const dev = mode === 'development';
 
 const config = {
 	plugins: [
+		//Use imports like SASS (include imports instead of referencing them)
+		atImport(),
+		//Nested syntax unwrapping like in sass
+		postcssNested(),
 		//Some plugins, like tailwindcss/nesting, need to run before Tailwind,
 		tailwindcss(),
+		//Use future CSS, today (transforms non-standardized CSS to more compatible CSS)
+		postcssPresetEnv(),
 		//But others, like autoprefixer, need to run after,
 		autoprefixer(),
 		!dev &&
--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,25 @@
 import preprocess from 'svelte-preprocess';
+import adapter from '@sveltejs/adapter-node';
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
+		adapter: adapter({
+			// default options are shown
+			out: 'build',
+			precompress: false,
+			env: {
+				host: 'HOST',
+				port: 'PORT'
+			}
+		}),
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte'
 	},
 
 	preprocess: [
 		preprocess({
+			defaults: { style: "postcss" },
 			postcss: true
 		})
 	]
